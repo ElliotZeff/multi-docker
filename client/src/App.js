ae458588e3e8e650a0b82403f6e94418f6d3bb9f
ElliotZeff <elliotzeff@gmail.com>
2021-12-14 17:06:09 +0000

    alter app.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ function App() {
           >
             BAYBAY!!!
           </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/">BAYBAY</Link>
+          <Link to="/otherpage">BAY BAYYYYYYY</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
